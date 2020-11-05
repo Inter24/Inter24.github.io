4a00e79cf8694cc46a7752732eee1fc6aeeb5079
Inter24 <web@bk-company.ru>
2020-11-05 16:16:47 +0300

    with prompt

--- js/main.js
+++ js/main.js
@@ -7,9 +7,19 @@ text.addEventListener('input', (event) => {
 });
 
 submit.addEventListener('click', (event) => {
-    
+
     event.preventDefault();  
     var message = document.querySelector('#text').value;  
     alert('Спасибо за нажатие на нашу замечательную кнопку. Значение вашего поля — ' + message);
+
+});
+
+submitprompt.addEventListener('click', (event) => {
+
+    event.preventDefault();  
+    const rubl = 78;     
+    let dollar = prompt('Введите доллары:');     
+    var result = rubl * dollar;
+    alert(result);
 
 });
