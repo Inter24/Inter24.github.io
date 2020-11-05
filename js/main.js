a1f176326fe796f00095442a91288189ac881c88
Inter24 <web@bk-company.ru>
2020-11-05 15:56:24 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 
 text.addEventListener('input', (event) => {
     
-    event.preventDefault()
+    event.preventDefault();    
     console.log(text.value);   
    
+});
+
+submit.addEventListener('click', (event) => {
+    event.preventDefault();  
+    var message = document.querySelector('#text').value;  
+    alert('Спасибо за нажатие на нашу замечательную кнопку. Значение вашего поля — ' + message);
+
 });
